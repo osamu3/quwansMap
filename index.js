c6228946bb293a73391bb9737dd66d4d5a6bc756
osamu3 <osamu@quwans.jp>
2017-11-23 18:59:29 +0900

    Veiw QwansMap prgram start

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
-var http = require('http');
+var http = require('http'),
+ fs = require('fs');
+var server = http.createServer();
 
-var server = http.createServer(function(request, response) {
-
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello osmu3 !!!");
-
+server.on('request',function(req, res) {
+	fs.readFile(__dirname+'/publicHtml/index.html','utf-8',function(err,htmlSorce){
+		if(err){
+			res.writeHead(404,{'Content-Type':'text/plain'});
+			res.write("index.html file not found!");
+			return res.end();
+		}
+		res.writeHead(200, {"Content-Type": "text/html"});
+		res.write(htmlSorce);
+  	res.end();
+	});
 });
 
 var port = process.env.PORT || 1337;
 server.listen(port);
 
-console.log("Server running at http://localhost:%d", port);
+console.log("Server running port:%d", port);

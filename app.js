823a35c2e861e07c1a61909c109ef96312960789
osamu3 <osamu@quwans.jp>
2017-11-25 10:49:25 +0900

    地図とシカと猪表示できた。

--- app.js
+++ app.js
@@ -5,46 +5,20 @@ var app = express();
 
 //Express での静的ファイルの提供 ←重要
 //http://expressjs.com/ja/starter/static-files.htmlより
-//Cf:app.use('/static', express.static('public'));←これで、public ディレクトリー内のファイルを /static パス・プレフィックスからロードできます。
+//Cf:app.use('/static', express.static('public'));←別名定義例：public ディレクトリー内のファイルを /static パス・プレフィックスからロードできます。
 app.use(express.static('public'));
+
 // (クライアント側設定)View EngineにEJSを指定。
 app.set('view engine', 'ejs');
 
-var fs = require('fs');
-
-var port = process.env.PORT || 1337;
-
 /* 2. listen()メソッドを実行してポートで待ち受け。*/
+var port = process.env.PORT || 1337;
 var server = app.listen(port, function(){
     console.log("Node.js is listening to PORT:" + server.address().port);
     console.log("dirName=" + __dirname);
 });
 
-
 // "/"へのGETリクエストで/views/index.ejsを表示する。拡張子（.ejs）は省略されていることに注意。
 app.get("/", function(req, res, next){
     res.render("index", {});
 });
-
-/*
-
-
-server.on('request',function(req, res) {
-	fs.readFile(__dirname+'/views/index.html','utf-8',function(err,htmlSorce){
-		if(err){
-			res.writeHead(404,{'Content-Type':'text/plain'});
-			res.write("index.html file not found!");
-			return res.end();
-		}
-    res.writeHead(200, {'Content-Type': 'text/html'});
-		res.write('Read File Success !!!');
-		//res.write(htmlSorce);
-  	res.end();
-	});
-});
-
-var port = process.env.PORT || 1337;
-server.listen(port);
-*/
-
-console.log("Server running port:%d", port);
--- public/js/initMap.js
+++ public/js/initMap.js
@@ -1,107 +1,74 @@
-// Google Maps APIのコールバック関数
-function initMap() {
-  var currentWindow = null,
-  markers = [],
-  infowindows = [],
-  markerCount = 0,
-  markerMax = 1000,// マーカー最大数
-  infowindowMax = 3;// フキダシ最大数
-  var map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 38.2586, lng: 137.6850},
-    zoom: 7,
-    zoomControlOptions: {
-        position: google.maps.ControlPosition.LEFT_BOTTOM
-    },
-    streetViewControlOptions: {
-        position: google.maps.ControlPosition.LEFT_BOTTOM
-    }
-  });
-  // 日本を含む矩形をfitBoundsすることで、画面サイズに依らず日本の中心を表示
-  //map.fitBounds(new google.maps.LatLngBounds(
-  //  new google.maps.LatLng(27.128750, 128.267736),
-  //  new google.maps.LatLng(44.489597, 144.263829)
-  //));
-  // Socket.IOと接続
-  var socket = io.connect();
-  // Streaming APIからデータが来た際の処理
-  socket.on('msg', function(data) {
-    if (currentWindow) {
-      currentWindow.close();
-    }
-    // 位置情報の名称があれば表示
-    if (data.place && data.place.name) {
-        text += '<hr><i class="fa fa-fw fa-map-marker text-danger"></i> <a href="https://twitter.com/search?q=place%' + data.place.id + '" target="_blank">' + data.place.name + '</a>';
-    }
-    text += '</div>';
-    // 地図マーカーのフキダシ
-    var infowindow = new google.maps.InfoWindow({
-      content: '<div>' + 'tweet' + '</div>',
-      maxWidth: 200,
-      disableAutoPan: true
-    });
-    infowindows.push(infowindow);
-    // 地図マーカー
-    var marker = new google.maps.Marker({
-      map: map,
-      position: new google.maps.LatLng(data.geo.coordinates[0], data.geo.coordinates[1]),
-      animation: google.maps.Animation.DROP,
-      title: '@' + data.user.screen_name
-    });
-    marker.infowindow = infowindow;
-    // マーカーをクリックしたらフキダシ表示
-    marker.addListener('click', function() {
-      this.infowindow.open(map, this);
-    });
-    // デフォルトでフキダシを開く
-    infowindow.open(map, marker);
-    markers.push(marker);
-    // ウインドウが最大数になったら前のものを閉じる
-    if (markerCount >= infowindowMax) {
-      var cls = markerCount - infowindowMax;
-      infowindows[cls].close();
-    }
-    // マーカー数を記録
-    markerCount++;
-    // マーカーが最大数になったら徐々に透明になっていく（10段階）
-    if (markerCount > markerMax) {
-      var fade = (markerCount + 1) - markerMax;
-      markers[fade].setOpacity(0.9);
-      fade--;
-      if (markers[fade]) {
-        markers[fade].setOpacity(0.8);
-        fade--;
-        if (markers[fade]) {
-          markers[fade].setOpacity(0.7);
-          fade--;
-          if (markers[fade]) {
-            markers[fade].setOpacity(0.6);
-            if (markers[fade]) {
-              markers[fade].setOpacity(0.5);
-              fade--;
-              if (markers[fade]) {
-                markers[fade].setOpacity(0.4);
-                fade--;
-                if (markers[fade]) {
-                  markers[fade].setOpacity(0.3);
-                  fade--;
-                  if (markers[fade]) {
-                    markers[fade].setOpacity(0.2);
-                    fade--;
-                    if (markers[fade]) {
-                      markers[fade].setOpacity(0.1);
-                      fade--;
-                      if (markers[fade]) {
-                        markers[fade].setMap(null);
-                        markers[fade] = null;
-                      }
-                    }
-                  }
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-  });
-}
+﻿$(function() {
+	var map = L.map('map'); //おまじない
+	L.tileLayer('https://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png', {
+		attribution: "<a href='https://maps.gis.go.jp/development/ichiran.html' target='_blank'>国土地理院タイル</a>",
+	}).addTo(map);
+
+	map.setView([35.23222265555128, 135.21938323974607],11);
+
+	L.control.scale({metric: true, imperial: false}).addTo(map);
+
+	map.on('click', function(e) {
+		L.marker([e.latlng.lat, e.latlng.lng], {
+			title: "シカ",
+			icon: shikaIcon
+		}).addTo(map).bindPopup("シカをクリックしました。");
+//		}).addTo(map).bindPopup("シカをクリックしました。").openPopup();
+
+
+		//データ追加
+		$("#dataRange").append(
+            $("<tr></tr>")
+                .append($("<td class='Lat'></td>").text(e.latlng.lat))
+                .append($("<td class='Lng'></td>").text(e.latlng.lng))
+        );
+
+	});
+
+
+	var inosisiIcon = L.icon({
+		iconUrl: 'img/inosisi.png',
+		iconRetinaUrl: 'img/inosisi.png',
+		iconSize: [50, 50],
+		iconAnchor: [25, 20],
+		popupAnchor: [0, -30]
+	});
+
+
+	var shikaIcon = L.icon({
+		iconUrl: 'img/shika.png',
+		iconRetinaUrl: 'img/shika.png',
+		iconSize: [50, 50],
+		iconAnchor: [25, 20],
+		popupAnchor: [0, -30]
+	});
+
+	//Jsonファイルの読み込み ファイル名=markers.json
+	$.getJSON("dataJson/markers.json", function(animals) {
+		for (var i in animals) {
+			var animal = animals[i];
+			if(animal.Type == "シカ"){
+				L.marker([animal.lat, animal.lng], {
+					title: "シカ",
+					icon: shikaIcon
+				}).addTo(map).on('click', function(e) {//setContent(animal.Type)とするとバグがでる。
+					L.popup()
+					.setLatLng([e.latlng.lat, e.latlng.lng])
+					.setContent("シカ")
+					.openOn(map);
+				});
+			}
+			if(animal.Type == "イノシシ"){
+				L.marker([animal.lat, animal.lng], {
+					title: "イノシシ",
+					icon: inosisiIcon
+				}).addTo(map).on('click', function(e) {
+					L.popup()
+					.setLatLng([e.latlng.lat, e.latlng.lng])
+					.setContent("イノシシ")
+					.openOn(map);
+				});
+			}
+		}
+	});
+});

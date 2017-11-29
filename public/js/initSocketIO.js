13407f720456f70ec12372427723bf16aa670af4
osamu3 <osamu@quwans.jp>
2017-11-29 14:52:35 +0900

    streetviw ok2

---
+++ public/js/initSocketIO.js
@@ -0,0 +1,17 @@
+﻿$(function() {
+//=======================socketIO=========================
+	var socket = io.connect();
+
+	socket.on('S2C_Msg', function (data) {
+		console.log('\nsocket.ontest.jsのsocket.on関数の呼び出しがありました。\n');
+		alert("サーバーからメッセージがありました。["+data+"]");
+	});
+
+	//===========ソケットIO(受信)イベント================= 
+	function sendMsg(msg) {
+		console.log('   サーバーへEmitしました。');
+		socket.emit('C2S_Msg', { "type":"mapClick","msg":msg });
+	}
+//================================================
+
+});
